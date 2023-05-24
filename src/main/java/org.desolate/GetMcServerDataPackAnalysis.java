e7e92a5817ab6779c5a21a5a302dc82f1fdcb7d6
voyage27 <476445136@qq.com>
2023-05-24 20:32:44 +0800

    Sync

--- src/main/java/org.desolate/GetMcServerDataPackAnalysis.java
+++ src/main/java/org.desolate/GetMcServerDataPackAnalysis.java
@@ -58,40 +58,45 @@ public class GetMcServerDataPackAnalysis {
     }
 
     //获取服务器状态(成功返回服务器信息数组，失败返回空字符串数组)
-    private String GetMcInfoDataPack() throws IOException {
-        //构建数据包输入参数
-        ByteArrayOutputStream byteArrayOutputStream_Input = new ByteArrayOutputStream();
-        DataOutputStream dataOutputStream_Input = new DataOutputStream(byteArrayOutputStream_Input);
+    private String GetMcInfoDataPack() {
+        try {
+            //构建数据包输入参数
+            ByteArrayOutputStream byteArrayOutputStream_Input = new ByteArrayOutputStream();
+            DataOutputStream dataOutputStream_Input = new DataOutputStream(byteArrayOutputStream_Input);
 
-        //构建数据包
-        dataOutputStream_Input.write(0x00);
-        dataOutputStream_Input.write(IntToVarInt(755));
-        dataOutputStream_Input.write(IntToVarInt(serverAddress.length()));
-        dataOutputStream_Input.writeBytes(serverAddress);
-        dataOutputStream_Input.writeShort(serverPort);
-        dataOutputStream_Input.write(IntToVarInt(0x01));
+            //构建数据包
+            dataOutputStream_Input.write(0x00);
+            dataOutputStream_Input.write(IntToVarInt(755));
+            dataOutputStream_Input.write(IntToVarInt(serverAddress.length()));
+            dataOutputStream_Input.writeBytes(serverAddress);
+            dataOutputStream_Input.writeShort(serverPort);
+            dataOutputStream_Input.write(IntToVarInt(0x01));
 
-        //与服务器建立Socket连接
-        Socket socket = new Socket(serverAddress, serverPort);
+            //与服务器建立Socket连接
+            Socket socket = new Socket(serverAddress, serverPort);
 
-        //构建服务器返回数据包接收参数
-        DataOutputStream dataOutputStream_Result = new DataOutputStream(socket.getOutputStream());
+            //构建服务器返回数据包接收参数
+            DataOutputStream dataOutputStream_Result = new DataOutputStream(socket.getOutputStream());
 
-        //重构数据包
-        dataOutputStream_Result.write(IntToVarInt(byteArrayOutputStream_Input.size()));
-        dataOutputStream_Result.write(byteArrayOutputStream_Input.toByteArray());
-        dataOutputStream_Result.writeByte(0x01);
-        dataOutputStream_Result.writeByte(0x00);
+            //重构数据包
+            dataOutputStream_Result.write(IntToVarInt(byteArrayOutputStream_Input.size()));
+            dataOutputStream_Result.write(byteArrayOutputStream_Input.toByteArray());
+            dataOutputStream_Result.writeByte(0x01);
+            dataOutputStream_Result.writeByte(0x00);
 
-        //解析数据包
-        DataInputStream dataInputStream_Result = new DataInputStream(socket.getInputStream());
-        readVarIntFromStream(dataInputStream_Result);
-        //数据包标志
-        //int FLAG = readVarIntFromStream(dataInputStream_Result);
-        //数据包长度
-        int LENGTH = readVarIntFromStream(dataInputStream_Result);
-        byte[] data = new byte[LENGTH];
-        dataInputStream_Result.readFully(data);
-        return new String(data, StandardCharsets.UTF_8);
+            //解析数据包
+            DataInputStream dataInputStream_Result = new DataInputStream(socket.getInputStream());
+            readVarIntFromStream(dataInputStream_Result);
+            //数据包标志
+            //int FLAG = readVarIntFromStream(dataInputStream_Result);
+            //数据包长度
+            int LENGTH = readVarIntFromStream(dataInputStream_Result);
+            byte[] data = new byte[LENGTH];
+            dataInputStream_Result.readFully(data);
+            return new String(data, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return new String("");
+        }
     }
 }
