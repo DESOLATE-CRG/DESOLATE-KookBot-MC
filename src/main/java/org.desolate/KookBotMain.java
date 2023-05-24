a72ecfe2c6c917c8efca15ac8f2bd71b54281d39
voyage27 <476445136@qq.com>
2023-05-24 15:25:04 +0800

    Add plugin framework

--- src/main/java/org.desolate/KookBotMain.java
+++ src/main/java/org.desolate/KookBotMain.java
@@ -1,4 +1,31 @@
 package org.desolate;
 
-public class KookBotMain {
+import snw.jkook.plugin.BasePlugin;
+
+public class KookBotMain extends BasePlugin{
+    private static KookBotMain instance;
+
+    @Override
+    public void onLoad() {
+        instance = this;
+        MyLogger("插件正在加载...");
+    }
+
+    @Override
+    public void onEnable() {
+        MyLogger("插件加载成功");
+    }
+
+    @Override
+    public void onDisable() {
+        MyLogger("插件正在关闭");
+    }
+
+    public static void MyLogger(String s) {
+        getInstance().getLogger().info("[GetMS] " + s);
+    }
+
+    public static KookBotMain getInstance() {
+        return instance;
+    }
 }
