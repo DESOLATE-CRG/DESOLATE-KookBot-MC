4a10d9025a0464c809b6865b08e13969d7423278
voyage27 <476445136@qq.com>
2023-06-11 14:56:05 +0800

    Add: 增加更换玩家信息绑定功能(Permission: Operator)

--- src/main/java/org.desolate/JsonFileOperate.java
+++ src/main/java/org.desolate/JsonFileOperate.java
@@ -10,7 +10,8 @@ import java.util.Objects;
 
 public class JsonFileOperate {
     private String dataFolderPath = "";
-    private final String fileName = "/PlayerDatabase.json";
+    private final String playerDBFileName = "/PlayerDatabase.json";
+    private final String administratorDBFileName = "/Administrator.json";
     private JSONObject currentClassPlayerInfo = new JSONObject();
 
     public void setDataFolderPath(String dataFolderPath) {
@@ -23,11 +24,11 @@ public class JsonFileOperate {
 
     public boolean DatabaseFileInit() {
         try {
-            File dbFile = new File(dataFolderPath + fileName);
+            File dbFile = new File(dataFolderPath + playerDBFileName);
             if (!dbFile.exists()) {
                 dbFile.createNewFile();
                 JSONArray jsonFileObj = new JSONArray();
-                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dataFolderPath + fileName));
+                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dataFolderPath + playerDBFileName));
                 fileWriter.write(jsonFileObj.toString());
                 fileWriter.close();
                 KookBotMain.MyLogger("数据库文件初始化成功");
@@ -45,7 +46,7 @@ public class JsonFileOperate {
     public boolean IfPlayerIsNoBinding(String playerKookID) {
         try {
             //读取文件
-            File targetFile = new File(dataFolderPath + fileName);
+            File targetFile = new File(dataFolderPath + playerDBFileName);
             FileReader fileReader = new FileReader(targetFile);
             Reader reader = new InputStreamReader(Files.newInputStream(targetFile.toPath()), StandardCharsets.UTF_8);
             int ch = 0;
@@ -77,7 +78,7 @@ public class JsonFileOperate {
     public boolean AddNewBinding(String playerName, String playerUUID, String playerKookID) {
         try {
             //读取数据库文件并解析
-            File jsonFile = new File(dataFolderPath + fileName);
+            File jsonFile = new File(dataFolderPath + playerDBFileName);
             FileReader fileReader = new FileReader(jsonFile);
             Reader reader = new InputStreamReader(Files.newInputStream(jsonFile.toPath()), StandardCharsets.UTF_8);
             int ch = 0;
@@ -88,21 +89,108 @@ public class JsonFileOperate {
             fileReader.close();
             reader.close();
             JSONArray jsonArrayObject = JSONArray.parse(stringBuffer.toString());
-            //构建新玩家数据模型
+            //构建新玩家数据信息
             JSONObject newPlayerObj = new JSONObject();
             newPlayerObj.put("playerName", playerName);
             newPlayerObj.put("playerUUID", playerUUID);
             newPlayerObj.put("playerKookID", playerKookID);
-            //写入主数据模型
+            //写入主数据库
             jsonArrayObject.add(newPlayerObj);
             //构建缓冲区写入流并实时写入文件
-            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dataFolderPath + fileName));
+            BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dataFolderPath + playerDBFileName));
             fileWriter.write(jsonArrayObject.toString());
             fileWriter.close();
             KookBotMain.MyLogger("检测到新写入内容，已更新文件");
             return true;
         } catch (IOException e) {
             KookBotMain.MyLogger("文件更新失败");
+            return false;
+        }
+    }
+
+    public boolean ChangePlayerBinding(String playerName, String playerNewName, String playerNewUUID) {
+        try {
+            //检测标志位
+            boolean flag = false;
+            //读取数据库文件并解析
+            File jsonFile = new File(dataFolderPath + playerDBFileName);
+            FileReader fileReader = new FileReader(jsonFile);
+            Reader reader = new InputStreamReader(Files.newInputStream(jsonFile.toPath()), StandardCharsets.UTF_8);
+            int ch = 0;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                stringBuffer.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            //构建JsonArrayObj
+            JSONArray playerDBObj = JSONArray.parse(stringBuffer.toString());
+            //保存玩家数组位置
+            int index = 0;
+            //保存原玩家绑定信息
+            JSONObject jsonObject = null;
+            for (Object item : playerDBObj) {
+                jsonObject = JSONObject.parse(item.toString());
+                if (Objects.equals(jsonObject.getString("playerName"), playerName)) {
+                    //检测到玩家信息
+                    flag = true;
+                    break;
+                }
+                index++;
+            }
+            if (flag) {
+                //删除原玩家绑定信息
+                playerDBObj.remove(index);
+                //构建新玩家数据模型
+                JSONObject newPlayerObj = new JSONObject();
+                newPlayerObj.put("playerName", playerNewName);
+                newPlayerObj.put("playerUUID", playerNewUUID);
+                newPlayerObj.put("playerKookID", jsonObject.getString("playerKookID"));
+                //写入主数据库
+                playerDBObj.add(newPlayerObj);
+                //保存更新数据库文件
+                BufferedWriter fileWriter = new BufferedWriter(new FileWriter(dataFolderPath + playerDBFileName));
+                fileWriter.write(playerDBObj.toString());
+                fileWriter.close();
+                KookBotMain.MyLogger("检测到新写入内容，已更新文件");
+                return true;
+            } else
+                return false;
+        } catch (IOException e) {
+            KookBotMain.MyLogger(e.getMessage());
+            return false;
+        }
+    }
+
+    public boolean IfPlayerHavePermission(String playerKookID) {
+        try {
+            //检测标志位
+            boolean flag = false;
+            //读取文件
+            File targetFile = new File(dataFolderPath + administratorDBFileName);
+            FileReader fileReader = new FileReader(targetFile);
+            Reader reader = new InputStreamReader(Files.newInputStream(targetFile.toPath()), StandardCharsets.UTF_8);
+            int ch = 0;
+            StringBuffer stringBuffer = new StringBuffer();
+            while ((ch = reader.read()) != -1) {
+                stringBuffer.append((char) ch);
+            }
+            fileReader.close();
+            reader.close();
+            //构造JSON对象
+            JSONObject administratorJsonObj = JSONObject.parse(stringBuffer.toString());
+            //取出管理员ID数组
+            JSONArray adArray = JSONArray.parse(administratorJsonObj.getString("adminArray"));
+            for (Object item : adArray) {
+                //检测到是管理员
+                if (Objects.equals(item, playerKookID)) {
+                    flag = true;
+                    break;
+                }
+            }
+            return flag;
+        } catch (IOException e) {
+            KookBotMain.MyLogger(e.getMessage());
             return false;
         }
     }
--- src/main/java/org.desolate/KookBotMain.java
+++ src/main/java/org.desolate/KookBotMain.java
@@ -23,9 +23,11 @@ public class KookBotMain extends BasePlugin {
     @Override
     public void onLoad() {
         instance = this;
+        MyLogger("插件开始加载...");
         //在插件加载时释放配置文件
         saveDefaultConfig();
-        MyLogger("插件正在加载...");
+        //释放权限配置文件
+        saveResource("Administrator.json", false, false);
     }
 
     @Override
@@ -33,7 +35,7 @@ public class KookBotMain extends BasePlugin {
         //设置全局配置
         getMcServerDataPackAnalysis.setServerAddress(getConfig().getString("ServerIPAddress"));
         getMcServerDataPackAnalysis.setServerPort(getConfig().getInt("ServerPort"));
-        analysisYmlFile.setYmlFilePath(getConfig().getString("YmlFilePath"));
+        analysisYmlFile.setYmlFilePath(getConfig().getString("PVPStateYmlFilePath"));
         jsonFileOperate.setDataFolderPath(getDataFolder().getPath());
         jsonFileOperate.DatabaseFileInit();
 
@@ -86,29 +88,29 @@ public class KookBotMain extends BasePlugin {
                             message.reply(HelpInfoCard);
                         }
                     } else if (arguments[0].toString().equalsIgnoreCase("bind") && arguments.length >= 2) {
-                        String PlayerName = (String) arguments[1];
-                        String PlayerUUID = "00000000-0000-0000-0000-000000000000";
+                        String playerName = (String) arguments[1];
+                        String playerUUID = "00000000-0000-0000-0000-000000000000";
                         //获取数据
                         JSONObject jsonDataPack = getMcServerDataPackAnalysis.getServerInfo();
                         JSONArray jsonArrayList = JSONArray.parse(jsonDataPack.getJSONObject("onlinePlayerList").getString("playerList"));
                         for (Object item : jsonArrayList) {
                             JSONObject temp = JSONObject.parse(item.toString());
-                            if (Objects.equals(temp.getString("name"), PlayerName)) {
-                                PlayerUUID = temp.getString("id");
+                            if (Objects.equals(temp.getString("name"), playerName)) {
+                                playerUUID = temp.getString("id");
                                 break;
                             }
                         }
                         //判玩家是否在线
-                        if (!Objects.equals(PlayerUUID, "00000000-0000-0000-0000-000000000000")) {
+                        if (!Objects.equals(playerUUID, "00000000-0000-0000-0000-000000000000")) {
                             //判玩家是否已绑定
                             if (jsonFileOperate.IfPlayerIsNoBinding(sender.getId())) {
                                 //添加新玩家绑定
-                                if (jsonFileOperate.AddNewBinding(PlayerName, PlayerUUID, sender.getId())) {
+                                if (jsonFileOperate.AddNewBinding(playerName, playerUUID, sender.getId())) {
                                     MultipleCardComponent BindCard = new CardBuilder()
                                             .setTheme(Theme.PRIMARY)
                                             .setSize(Size.LG)
                                             .addModule(new HeaderModule(new PlainTextElement("DESOLATE-MC Server", false)))
-                                            .addModule(new SectionModule(new PlainTextElement("KOOK用户: " + senderName + "\n游戏账户: " + PlayerName + "\n绑定成功!"), null, null))
+                                            .addModule(new SectionModule(new PlainTextElement("KOOK用户: " + senderName + "\n游戏账户: " + playerName + "\n绑定成功!"), null, null))
                                             .build();
                                     if (message != null) {
                                         message.reply(BindCard);
@@ -155,8 +157,42 @@ public class KookBotMain extends BasePlugin {
                                 message.reply("检测到你没有绑定游戏账户哦，快快去绑定一个叭！");
                             }
                         }
-                    } else if (arguments[0].toString().equalsIgnoreCase("changeBind") && arguments.length >= 2) {
-
+                    } else if (arguments[0].toString().equalsIgnoreCase("changeBind") && arguments.length >= 3) {
+                        if (jsonFileOperate.IfPlayerHavePermission(sender.getId())) {
+                            String playerName = (String) arguments[2];
+                            String newPlayerName = (String) arguments[1];
+                            String playerUUID = "00000000-0000-0000-0000-000000000000";
+                            //获取数据
+                            JSONObject jsonDataPack = getMcServerDataPackAnalysis.getServerInfo();
+                            JSONArray jsonArrayList = JSONArray.parse(jsonDataPack.getJSONObject("onlinePlayerList").getString("playerList"));
+                            for (Object item : jsonArrayList) {
+                                JSONObject temp = JSONObject.parse(item.toString());
+                                if (Objects.equals(temp.getString("name"), playerName)) {
+                                    playerUUID = temp.getString("id");
+                                    break;
+                                }
+                            }
+                            //判玩家是否在线
+                            if (!Objects.equals(playerUUID, "00000000-0000-0000-0000-000000000000")) {
+                                if (jsonFileOperate.ChangePlayerBinding((String) arguments[1], playerName, playerUUID)) {
+                                    if (message != null) {
+                                        message.reply("换绑成功！");
+                                    }
+                                } else {
+                                    if (message != null) {
+                                        message.reply("未检测到" + newPlayerName + "的绑定信息");
+                                    }
+                                }
+                            } else {
+                                if (message != null) {
+                                    message.reply("此玩家当前不在线！");
+                                }
+                            }
+                        } else {
+                            if (message != null) {
+                                message.reply("您没有管理权限，请联系管理员为您换绑！");
+                            }
+                        }
                     }
                 }).register(this);
         MyLogger("插件加载成功");
